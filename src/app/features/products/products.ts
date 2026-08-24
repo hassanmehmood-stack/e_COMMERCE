0ba4a85e05f7964c91c31669f3c77a8026e309a2
Hassan <hassan.mehmood@visnext.net>
2026-08-24 14:29:31 +0500

    Implement and setup the routes , header and footer

---
+++ src/app/features/products/products.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-products',
+  styleUrl: './products.scss',
+  templateUrl: './products.html',
+})
+export class Products {}
